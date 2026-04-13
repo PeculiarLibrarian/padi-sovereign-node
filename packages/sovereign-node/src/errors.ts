b060b1847af457f8c7f60fe65d9cc5b7020b2638
S. M. Gitandu, B.S. <peculiarlibrarian@gmail.com>
2026-04-13 12:00:21 +0300

    Update errors.ts

--- packages/sovereign-node/src/errors.ts
+++ packages/sovereign-node/src/errors.ts
@@ -1,19 +1,31 @@
-export type ErrorCode =
-  | "CANON_UNDEFINED" | "CANON_NON_FINITE" | "CANON_NON_PLAIN_OBJECT"
-  | "AUTH_SIGNATURE_INVALID" | "AUTH_KEY_TYPE_VIOLATION" | "AUTH_REQUIRED"
-  | "CHAIN_HASH_MISMATCH" | "CHAIN_BROKEN_PARENT" | "CHAIN_HEIGHT_GAP"
-  | "CHAIN_ORPHAN" | "CHAIN_MULTI_PARENT"
-  | "EPOCH_MISMATCH" | "EPOCH_REGRESSION" | "EPOCH_STALE_BLOCK"
-  | "SCHEMA_INVALID" | "SCHEMA_MISSING_CONTEXT" | "SCHEMA_UNKNOWN_SHAPE"
-  | "SCHEMA_MISSING_FIELD" | "SCHEMA_MAX_VIOLATION" | "SCHEMA_MIN_VIOLATION"
-  | "LEADER_INGEST_LOCK" | "LEADER_FENCED" | "LEADER_NOT_ELIGIBLE"
-  | "REPLAY_NONCE_DUPLICATE" | "BACKFILL_LIMIT_EXCEEDED" | "BACKFILL_STRUCT_INVALID"
-  | "SYSTEM_FUTURE_DRIFT" | "SYSTEM_CONFIG_ERROR";
+export type ErrorCode = 
+  | "AUTH_SIGNATURE_INVALID" 
+  | "AUTH_KEY_TYPE_VIOLATION" 
+  | "AUTH_REQUIRED" 
+  | "LEADER_INGEST_LOCK" 
+  | "NONCE_REPLAY" 
+  | "SCHEMA_VIOLATION";
 
 export class PadiError extends Error {
-  constructor(public readonly code: ErrorCode, message?: string) {
-    super(message ?? code);
-    this.name = "PadiError";
+    constructor(public code: ErrorCode, message?: string) {
+        super(message || code);
+        this.name = "PadiError";
+    }
+}
+
+export function httpStatusForError(code: ErrorCode): number {
+  switch (code) {
+    case "AUTH_SIGNATURE_INVALID":
+    case "AUTH_KEY_TYPE_VIOLATION":
+    case "AUTH_REQUIRED":
+      return 401;
+    case "NONCE_REPLAY":
+      return 403;
+    case "SCHEMA_VIOLATION":
+      return 400;
+    case "LEADER_INGEST_LOCK":
+      return 409;
+    default:
+      return 500;
   }
-  toJSON() { return { code: this.code, message: this.message }; }
 }
